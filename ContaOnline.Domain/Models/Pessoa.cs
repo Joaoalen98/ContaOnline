9795ca29ad26006d5d0c8269e70097c730c3bc3b
João Alencar <joao.alencar1497@gmail.com>
2023-03-14 22:52:43 -0300

    colocado campo de data de nascimento como nullable

--- ContaOnline.Domain/Models/Pessoa.cs
+++ ContaOnline.Domain/Models/Pessoa.cs
@@ -6,6 +6,6 @@ namespace ContaOnline.Domain.Models
     {
         public string CPF { get; set; }
         public string RG { get; set; }
-        public DateTime DataNascimento { get; set; }
+        public DateTime? DataNascimento { get; set; }
     }
 }
