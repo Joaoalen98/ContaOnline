bd622795e1b1afd7aa9a8b690bac70fa67719a98
João Alencar <joao.alencar1497@gmail.com>
2023-03-23 21:05:53 -0300

    criado tela de incluir contato

---
+++ ContaOnline.UI.Web/Controllers/ContatoController.cs
@@ -0,0 +1,25 @@
+﻿using ContaOnline.Domain.Interfaces;
+using ContaOnline.Domain.Models;
+using ContaOnline.UI.Web.Models;
+using System.Web.Mvc;
+
+namespace ContaOnline.UI.Web.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly IContatoRepository _contatoRepository;
+        private readonly Usuario _usuario;
+
+        public ContatoController()
+        {
+            _contatoRepository = AppHelper.ObterContatoRepository();
+            _usuario = AppHelper.ObterUsuarioLogado();
+        }
+
+        public ActionResult Incluir()
+        {
+            var contatoViewModel = new ContatoViewModel();
+            return View(contatoViewModel);
+        }
+    }
+}
